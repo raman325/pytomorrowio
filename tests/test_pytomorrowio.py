0735b39ffade347cdaffe6fb5e6aeaa7374243f4
Lyman Epp <lyman@epptech.com>
2022-04-14 20:41:50 -0400

    Test cleanup

--- tests/test_pytomorrowio.py
+++ tests/test_pytomorrowio.py
@@ -64,8 +64,8 @@ def create_trace_config() -> TraceConfig:
     return trace_config
 
 
-def mock_call_api():
-    return patch("pytomorrowio.TomorrowioV4._call_api")
+def create_call_api_mock():
+    return patch.object(TomorrowioV4, "_call_api")
 
 
 def load_json(file_name: str):
@@ -85,7 +85,7 @@ async def _test_capture_request_and_response():
 
 
 async def test_timelines_hourly_good():
-    with mock_call_api() as mock:
+    with create_call_api_mock() as mock:
         mock.return_value = load_json("timelines_hourly_good.json")
 
         api = TomorrowioV4("bogus_api_key", *GPS_COORD)
@@ -95,45 +95,46 @@ async def test_timelines_hourly_good():
         res = await api.forecast_hourly(available_fields)
         mock.assert_called_once()
 
-        assert res is not None
-        assert isinstance(res, Mapping)
+    assert res is not None
+    assert isinstance(res, Mapping)
 
-        data = res.get("data")
-        assert isinstance(data, Mapping)
+    data = res.get("data")
+    assert isinstance(data, Mapping)
 
-        timelines = data.get("timelines")
-        assert isinstance(timelines, Sequence)
-        assert len(timelines) == 1
+    timelines = data.get("timelines")
+    assert isinstance(timelines, Sequence)
+    assert len(timelines) == 1
 
-        timeline = timelines[0]
-        assert isinstance(timeline, Mapping)
+    timeline = timelines[0]
+    assert isinstance(timeline, Mapping)
 
-        assert timeline.get("timestep") == TIMESTEP_HOURLY
+    assert timeline.get("timestep") == TIMESTEP_HOURLY
 
-        intervals = timeline.get("intervals")
-        assert isinstance(intervals, Sequence)
-        assert len(intervals) == 109
+    intervals = timeline.get("intervals")
+    assert isinstance(intervals, Sequence)
+    assert len(intervals) == 109
 
-        for interval in intervals:
-            assert isinstance(interval, Mapping)
+    for interval in intervals:
+        assert isinstance(interval, Mapping)
 
-            start_time = interval.get("startTime")
-            assert isinstance(start_time, str)
+        start_time = interval.get("startTime")
+        assert isinstance(start_time, str)
 
-            # Might be fixed in Python 3.11 - https://github.com/python/cpython/issues/80010
-            # Maybe pytomorrowio should do the replacements to improve user experience
-            start_time = re.sub("Z$", "+00:00", start_time)
-            datetime.fromisoformat(start_time)
+        # Might be fixed in Python 3.11 - https://github.com/python/cpython/issues/80010
+        # Maybe pytomorrowio should do the replacements to improve user experience
+        start_time = re.sub("Z$", "+00:00", start_time)
+        datetime.fromisoformat(start_time)
 
-            values = interval.get("values")
-            assert isinstance(values, Mapping)
+        values = interval.get("values")
+        assert isinstance(values, Mapping)
 
-            assert set(values) == set(available_fields)
+        assert set(values) == set(available_fields)
 
 
 async def test_timelines_daily_good():
-    with mock_call_api() as mock:
+    with create_call_api_mock() as mock:
         mock.return_value = load_json("timelines_daily_good.json")
+
         api = TomorrowioV4("bogus_api_key", *GPS_COORD)
         available_fields = api.available_fields(
             ONE_DAY, [TYPE_POLLEN, TYPE_PRECIPITATION, TYPE_WEATHER]
@@ -141,34 +142,34 @@ async def test_timelines_daily_good():
         res = await api.forecast_daily(available_fields)
         mock.assert_called_once()
 
-        assert res is not None
-        assert isinstance(res, Mapping)
+    assert res is not None
+    assert isinstance(res, Mapping)
 
-        data = res.get("data")
-        assert isinstance(data, Mapping)
+    data = res.get("data")
+    assert isinstance(data, Mapping)
 
-        timelines = data.get("timelines")
-        assert isinstance(timelines, Sequence)
-        assert len(timelines) == 1
+    timelines = data.get("timelines")
+    assert isinstance(timelines, Sequence)
+    assert len(timelines) == 1
 
-        timeline = timelines[0]
-        assert isinstance(timeline, Mapping)
+    timeline = timelines[0]
+    assert isinstance(timeline, Mapping)
 
-        assert timeline.get("timestep") == TIMESTEP_DAILY
+    assert timeline.get("timestep") == TIMESTEP_DAILY
 
-        intervals = timeline.get("intervals")
-        assert isinstance(intervals, Sequence)
-        assert len(intervals) == 15
+    intervals = timeline.get("intervals")
+    assert isinstance(intervals, Sequence)
+    assert len(intervals) == 15
 
-        expected_values = api.convert_fields_to_measurements(available_fields)
+    expected_values = api.convert_fields_to_measurements(available_fields)
 
-        for interval in intervals:
-            assert isinstance(interval, Mapping)
+    for interval in intervals:
+        assert isinstance(interval, Mapping)
 
-            start_time = interval.get("startTime")
-            assert isinstance(start_time, str)
+        start_time = interval.get("startTime")
+        assert isinstance(start_time, str)
 
-            values = interval.get("values")
-            assert isinstance(values, Mapping)
+        values = interval.get("values")
+        assert isinstance(values, Mapping)
 
-            assert set(values) == set(expected_values)
+        assert set(values) == set(expected_values)
