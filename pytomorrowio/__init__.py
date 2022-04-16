0f401d668b1bd311a7ce632fda6be847d90da912
Raman Gupta <7243222+raman325@users.noreply.github.com>
2022-04-15 21:52:26 -0400

    Bump version: 0.2.0 → 0.2.1

--- pytomorrowio/__init__.py
+++ pytomorrowio/__init__.py
@@ -4,4 +4,4 @@ from .pytomorrowio import TomorrowioV4, TomorrowioV4Sync
 
 __author__ = """raman325"""
 __email__ = "7243222+raman325@users.noreply.github.com"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
