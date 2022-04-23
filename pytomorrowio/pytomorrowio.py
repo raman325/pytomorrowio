ea9ba5961e8d8abbed0e2daa82738b98ed866e7c
Raman Gupta <7243222+raman325@users.noreply.github.com>
2022-04-22 23:20:26 -0400

    Use info log level for API warnings because they are not blocking (#23)
    * Use info log level for API warnings because they are not blocking
    
    * Improve message
    
    * black

--- pytomorrowio/pytomorrowio.py
+++ pytomorrowio/pytomorrowio.py
@@ -214,16 +214,13 @@ class TomorrowioV4:
 
         if resp.status == HTTPStatus.OK:
             self._num_api_requests += 1
-            # ignore "range for timestep not valid warning"
             for warning in set(
-                warning["message"]
-                for warning in resp_json.get("warnings", [])
-                if warning["code"] != 246009
+                warning["message"] for warning in resp_json.get("warnings", [])
             ):
-                _LOGGER.warning(
+                _LOGGER.info(
                     (
-                        "While calling the API for the following timesteps: [%s], the "
-                        "following warning was returned: %s"
+                        "While calling the API for the timesteps [%s], the following "
+                        "warning was returned: %s"
                     ),
                     ", ".join(params["timesteps"]),
                     warning,
