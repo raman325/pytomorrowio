8595833bf3ff6273d883106d5dbdcd7ab2fcae1d
Raman Gupta <7243222+raman325@users.noreply.github.com>
2023-07-16 02:48:33 -0400

    Bump version: 0.3.5 → 0.3.6

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/raman325/pytomorrowio",
-    version="0.3.5",
+    version="0.3.6",
     zip_safe=False,
 )
